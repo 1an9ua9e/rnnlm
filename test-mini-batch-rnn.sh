8fb2ddc715aad6fd117a0005c0826e9acf746862
1an9ua9e <pseudo.intelli9ence@gmail.com>
2017-09-06 13:41:24 +0900

    削除ファイルを反映

--- test-mini-batch-rnn.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-dir=$HOME/rnnlm
-pyenv global 3.6.0
-#python $dir/rnnlm.py --word_dim 5000 --hidden_dim 30 --class_dim 30 --class_type 0 --batch_size 15 --epoch 10 --sort 1 --data_size 1000
-python $dir/rnnlm.py --word_dim 5000 --hidden_dim 50 --class_dim -1 --class_type 0 --batch_size 15 --epoch 5 --sort 1 --data_size 1000
